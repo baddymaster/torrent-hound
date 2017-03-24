5318b189d5f77e1f3636f12eb05c5c5a4f5b95ce
Yashovardhan Sharma <yashovardhan13121@iiitd.ac.in>
2017-03-24 16:25:53 +0530

    Add License info to Source files and binaries

--- torrent-hound.py
+++ torrent-hound.py
@@ -1,3 +1,19 @@
+#   <Torrent Hound - Search torrents from multiple websites via the CLI.>
+#    Copyright (C) <2017>  <Yashovardhan Sharma>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as published
+#     by the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/env python
 # @author : Yashovardhan Sharma
 # @github : github.com/baddymaster
